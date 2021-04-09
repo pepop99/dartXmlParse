475472b1706e98a93e33cf318363b0af156c78da
Rishabh Arora <rishabharora1780@gmail.com>
2021-04-10 01:48:14 +0530

    optimized license getter

---
+++ lib/src/getLicense.dart
@@ -0,0 +1,43 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import 'normalize.dart';
+import 'package:xml/xml.dart';
+
+import 'parseXml.dart';
+
+void getLicense() async{
+  //url is being for testing purposes only, later a tar file will be used which will be searched for LICENSE file
+  var url = Uri.parse('https://raw.githubusercontent.com/spdx/license-list-XML/master/src/BSD-2-Clause-FreeBSD.xml');
+  var request = http.Request('GET', url);
+  
+  var response = await request.send();
+  
+  if (response.statusCode == 200) {
+    //code to store the streamed response as a string
+    var responseString = '';
+    await for (var bytes in response.stream.transform(Utf8Decoder())){
+      responseString += bytes;
+    }
+    
+    var xmlString = responseString;
+    //initializing the parser
+    var parser = xmlParser(xmlString);
+
+    var licenseName = parser.parse('name');
+    var licenseId = parser.parse('licenseId');
+    var licenseBody = parser.extractText();
+    
+    print('License Name: $licenseName');
+    print('License SPDX identifier: $licenseId');
+    print('Stripped down text: $licenseBody');
+
+    // The normalizer will normalize the parsed string according to SPDX guidlines
+    // var normalizer = Normalize(parsedString);
+  }
+  else {
+    print(response.reasonPhrase);
+  }
+}
+void main() {
+  getLicense();
+}
--- lib/src/parseXml.dart
+++ lib/src/parseXml.dart
@@ -1,32 +1,44 @@
 import 'package:xml/xml.dart';
 
 class xmlParser {
-  XmlNode baseNode;
+  String xmlString;
 
   //constructor to initialze class variables
-  xmlParser(XmlNode baseNode){
-    this.baseNode = baseNode;
+  xmlParser(String xmlString){
+    this.xmlString = xmlString;
   }
 
-  //function to begin parsing
-  String parse(){
-    var buffer = StringBuffer();
-    checkSubNode(buffer, baseNode);
-    var parsedString = buffer.toString();
-    return parsedString;
+  //function to get info of a particular property
+  String parse(String property){
+    var matchRegex = RegExp('(?<=$property=")(.*?)(?=")');
+    var match = matchRegex.stringMatch(xmlString);
+    return match;
   }
 
-  //helper function which will be called on every subnode of a node
-  void checkSubNode(StringBuffer buffer, XmlNode baseNode){
-    if(baseNode.nodeType == XmlNodeType.TEXT){
-      var nodeText = baseNode.text;
-      var trimmedText = nodeText.replaceAll(RegExp('[\n][ ]{2,}'), '').trim();
-      if(trimmedText.isNotEmpty){
-        buffer.write('$trimmedText\n');
+  //function to extract text from the license which will later be normalized and used to match licenses
+  String extractText() {
+    var temp = xmlString;
+    temp = temp.replaceAll(RegExp(r'\n'), '');
+    temp = temp.replaceAll(RegExp(r'\r'), '');
+    // temp = temp.replaceAll(RegExp(r'\s{2,}'), ' ');
+    var exp = RegExp(r'<text>(.*)<\/text>');
+    var matches = exp.allMatches(temp);
+    if(matches.isNotEmpty) {
+      var match = matches.elementAt(0);
+      if(match != null) {
+        var m = match.group(0);
+        m = m.replaceAll(RegExp(r'<copyrightText>.*?<\/copyrightText>'), '');
+        m = m.replaceAll(RegExp(r'<titleText>.*?<\/titleText>'), '');
+        m = m.replaceAll(RegExp(r'<optionalText>.*?<\/optionalText>'), '');
+        m = m.replaceAll(RegExp(r'<.*?>'), '');
+        m = m.replaceAll(RegExp(r'[^a-zA-Z0-9 ]'), ' ');
+        m = m.replaceAll(RegExp(r'\s{2,}'), ' ');
+        m = m.trim();
+        m = m.toLowerCase();
+        temp = m;
       }
     }
-    for(var node in baseNode.nodes){
-      checkSubNode(buffer, node);
-    }
+    return temp;
   }
+
 }
